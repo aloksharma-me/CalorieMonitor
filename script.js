744c247406d60176497e19e2a3cf7bbb2af18a7a
aloksharma-me <alok.sharma104@gmail.com>
2024-08-17 16:14:01 +0530

    added JavaScript TODOs

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// TODO: user inputs and change content based on that
+// TODO: add meals (show/hide details fields)
+// TODO: submit.onclick, clear.onclick
+// TODO: green and red (change background colour, text, icon color)
+// TODO: calculate net calories
+// TODO: form validation (text and number fields)
+
 console.log('diet guage');

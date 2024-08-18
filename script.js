418494d0600410c9ddddd45c6cd60ac2084cfaed
aloksharma-me <alok.sharma104@gmail.com>
2024-08-18 13:21:30 +0530

    wrote and tested code for getting calorie inputs on Calculate button click

--- script.js
+++ script.js
@@ -4,15 +4,34 @@
 // TODO: green and red (change background colour, text, icon color)
 // TODO: calculate net calories
 // TODO: form validation (text and number fields)
+//NOTE TO SELF: .value should be inside the onclick function for the value to be captured
 
-// Taking user inputs from fields calorie goal, breakfast calorie, lunch calorie, snacks calorie, dinner calorie, exercise calorie
 
-const submitButton = document.querySelector("#submit");
-const calorieGoal = document.querySelector("#calorieGoal").value;
+// stores references to all calorie fields
+let calorieGoal = document.querySelector("#calorieGoal");
+let calorieBreakfast  = document.querySelector("#calorieBreakfast");
+let calorieLunch = document.querySelector("#calorieLunch");
+let calorieSnacks = document.querySelector("#calorieSnacks");
+let calorieDinner = document.querySelector("#calorieDinner");
+let calorieExercise = document.querySelector("#calorieExercise");
 
-// submitButton.onclick = buttonClicked;
-submitButton.addEventListener("click", buttonClicked);
+// calculate button reference and event listener 
+const calculateButton = document.querySelector("#submit");
+calculateButton.addEventListener("click", calculateCalories);
 
-function buttonClicked(){
-    console.log(calorieGoal);
+// calculates calorie excess/deficit on Calculate button click
+function calculateCalories(){
+    getCalorieValues();
+    return console.log(calorieGoal, calorieBreakfast, calorieLunch, calorieSnacks, calorieDinner, calorieExercise);
+}
+
+// stores user entered calorie values on Calculate button click
+function getCalorieValues(){
+    calorieGoal = calorieGoal.value;
+    calorieBreakfast  = calorieBreakfast.value;
+    calorieLunch  = calorieLunch.value;
+    calorieSnacks  = calorieSnacks.value;
+    calorieDinner  = calorieDinner.value;
+    calorieExercise  = calorieExercise.value;
+    return calorieGoal, calorieBreakfast, calorieLunch, calorieSnacks, calorieDinner, calorieExercise;
 }

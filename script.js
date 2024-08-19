45cb001b239608d50f06f8ece443623d23d5f0b3
aloksharma-me <alok.sharma104@gmail.com>
2024-08-19 18:06:57 +0530

    logic is not working properly, may have to rebuild from scratch

--- script.js
+++ script.js
@@ -2,22 +2,32 @@
 // TODO: add meals (show/hide details fields)
 // DONE: submit.onclick, clear.onclick
 // TODO: green and red (change background colour, text, icon color)
-// TODO: calculate net calories
+// DONE: calculate net calories
 // TODO: form validation (text and number fields)
 //NOTE TO SELF: .value should be inside the onclick function for the value to be captured
 
 
-// stores references to all calorie fields
+// DOM references and calorie calculation variables
 let calorieGoal = document.querySelector("#calorieGoal");
 let calorieBreakfast  = document.querySelector("#calorieBreakfast");
 let calorieLunch = document.querySelector("#calorieLunch");
 let calorieSnacks = document.querySelector("#calorieSnacks");
 let calorieDinner = document.querySelector("#calorieDinner");
 let calorieExercise = document.querySelector("#calorieExercise");
+
+let addBreakfast = document.querySelector("#addBreakfast");
+let addLunch = document.querySelector("#addLunch");
+let addSnacks = document.querySelector("#addSnacks");
+let addDinner = document.querySelector("#addDinner");
+
+let breakfastDetails = document.querySelector(".breakfastDetails");
+let lunchDetails = document.querySelector(".lunchDetails");
+let snacksDetails = document.querySelector(".snacksDetails");
+let dinnerDetails = document.querySelector(".dinnerDetails");
+
 let calorieNet = 0;
 let calorieConsumed = 0;
 let calorieBurned = 0;
-    
 
 // calculateButton reference and call to calculateNetCalories function onclick
 const calculateButton = document.querySelector("#submit");
@@ -27,17 +37,51 @@ calculateButton.addEventListener("click", calculateNetCalories);
 const clearButton = document.querySelector("#clear");
 clearButton.addEventListener("click", clearPage);
 
+// add meal button interaction
+addBreakfast.addEventListener("click", showBreakfastDetails);
+addLunch.addEventListener("click", showLunchDetails);
+addSnacks.addEventListener("click", showSnacksDetails);
+addDinner.addEventListener("click", showDinnerDetails);
 
 /* FUNCTION DEFINITIONS BEGIN */
+
+// checks the current status of visibility, then toggles it
+function showBreakfastDetails(){
+    if(breakfastDetails.style.display === "block"){
+        breakfastDetails.style.display = "none";
+    } else{
+        breakfastDetails.style.display = "block";
+    }
+}
+function showLunchDetails(){
+    if(lunchDetails.style.display === "block"){
+        lunchDetails.style.display = "none";
+    } else{
+        lunchDetails.style.display = "block";
+    }
+}
+function showSnacksDetails(){
+    if(snacksDetails.style.display === "block"){
+        snacksDetails.style.display = "none";
+    } else{
+        snacksDetails.style.display = "block";
+    }
+}
+function showDinnerDetails(){
+    if(dinnerDetails.style.display === "block"){
+        dinnerDetails.style.display = "none";
+    } else{
+        dinnerDetails.style.display = "block";
+    }
+}
 
 // calculates calorie excess/deficit on Calculate button click
 function calculateNetCalories(){
     getCalorieValues();
-
+    // document.querySelector("#summaryCard").style.backgroundColor = "#15803D";
     calorieConsumed = (calorieBreakfast + calorieLunch + calorieSnacks + calorieDinner);
     calorieBurned = calorieExercise;
     calorieNet = calorieConsumed - calorieBurned;
-
     return console.log(calorieNet);
 }
 
@@ -58,5 +102,4 @@ function getCalorieValues(){
     
     return calorieGoal, calorieBreakfast, calorieLunch, calorieSnacks, calorieDinner, calorieExercise;
 }
-
 /* FUNCTION DEFINITIONS END */
